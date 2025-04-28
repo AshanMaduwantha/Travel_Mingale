75b4ca3beed1766640f8f7e15bea8b5efe583c2d
Oshadhie Indudunu <153111583+Oshadhie@users.noreply.github.com>
2025-04-28 15:38:15 +0530

    Create Room Model

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -4,8 +4,9 @@ import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 
 import connectDB from './config/mongodb.js';
-import authRouter from './routes/authRoutes.js';
-import userRouter from './routes/userRoutes.js';
+//import authRouter from './routes/authRoutes.js';
+//import userRouter from './routes/userRoutes.js';
+import hotelsRouter from "./routes/hotels.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -19,8 +20,8 @@ app.use(cors({origin: allowedOrigins, credentials: true}));
 
 //API endpoint
 app.get('/', (req, res) => res.send('API Working!'));
-app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter);
+//app.use('/api/auth', authRouter);
+//app.use('/api/user', userRouter);
 
 //Oshadhie 
 app.use("/api/hotels", hotelsRouter);
