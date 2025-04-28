6e45d7bdea1fad843cb0062e8218709bc4b09bdc
Oshadhie Indudunu <153111583+Oshadhie@users.noreply.github.com>
2025-04-28 16:20:53 +0530

    Create User Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,7 @@ import ResetPassword from './pages/ResetPassword'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Profile from './pages/Profile'
-import AdminHome from './pages/home/AdminHome';
-import NewHotel from './pages/hotel/newHotel/NewHotel';
-import UpdateHotel from './pages/hotel/updateHotel/UpdateHotel';
-import UpdateRoom from './pages/room/updateRoom/UpdateRoom';
+
 
 const App = () => {
   return (
@@ -24,10 +21,7 @@ const App = () => {
         <Route path='/profile' element={<Profile/>}/>
 
         /* Oshadhie routes */
-        <Route path='/admin' element={<AdminHome />} />
-          <Route path='/admin/add-hotel' element={<NewHotel />} />
-          <Route path='/admin/edit-hotel' element={< UpdateHotel/>} />
-          <Route path='/admin/edit-hotel' element={< UpdateRoom/>} />
+        
       </Routes>
     </div>
   )
