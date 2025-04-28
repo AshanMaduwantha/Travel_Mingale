29bac01ce8aeb3a0ed9434bfc873d7bb4fc00a1b
AshanMaduwantha <ashanofficial256@gmail.com>
2025-04-28 13:20:54 +0530

    create user model

---
+++ BACKEND/models/userModel.js
@@ -0,0 +1,70 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  verifyOtp: {
+    type: String,
+    default: ''
+  },
+
+  verifyOtpExpireAt: {
+    type: Number,
+    default: 0
+  },
+
+  isAccountVerified: {
+    type: Boolean,
+    default: false
+  },
+
+  resetOtp: {
+    type: String,
+    default: ''
+  },
+
+  resetOtpExpireAt: {
+    type: Number,
+    default: 0
+  },
+
+  // ✅ New Fields
+  phone: {
+    type: String,
+    default: ''
+  },
+
+  birthday: {
+    type: Date,
+    default: null
+  },
+
+  gender: {
+    type: String,
+    enum: ['Male', 'Female', 'Other'],
+    default: 'Other'
+  },
+
+  address: {
+    type: String,
+    default: ''
+  }
+});
+
+const userModel = mongoose.models.user || mongoose.model('user', userSchema);
+
+export default userModel;
