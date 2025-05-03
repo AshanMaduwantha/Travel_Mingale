ba7466ad84f71c20f5eed2c02c7b32be8573bef0
KavinduFndo <kavindusf2003@gmail.com>
2025-05-03 14:05:34 +0530

    update

--- frontend/src/pages/BookedReservation.jsx
+++ frontend/src/pages/BookedReservation.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify'; 
 import 'react-toastify/dist/ReactToastify.css'; 
 import ReactModal from 'react-modal'; 
-import Navbar from '../components/Navbar';
+
 
 ReactModal.setAppElement('#root'); 
 
